588ab551fa8fde2115b3525e396e71414b9c263b
JuanK003 <juankdev003@gmail.com>
2022-12-20 17:55:24 -0600

    Number

---
+++ 06_numbers.py
@@ -0,0 +1,28 @@
+# Printing the type of the variable age (INTEGER).
+age = 19
+print(type(age))
+
+# Printing the type of the variable temperature (FLOAT).
+temperature = 12.12
+print(type(temperature))
+
+# Subtracting 1 from lives and then adding 2 to lives.
+lives = 5
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives += 2
+print(lives)
+
+# Printing the number 550000000000000000000.1
+number = 550000000000000000000.1
+print(number)
+
+# Printing the number 0.0000000000000001.
+number2 = 0.0000000000000001
+print(number2)
