a0a9bbe324b4a32c1c58acd6c68ce82d713996f1
JuanK003 <juankdev003@gmail.com>
2022-12-20 17:34:34 -0600

    Data Types

---
+++ 04_data_types.py
@@ -0,0 +1,22 @@
+# DATA TYPES 
+# Assigning the value 'Juan Carlos' to the variable my_name, is a data type STRING. 
+my_name = 'Juan Carlos' 
+
+# Printing the data type of the variable my_name.
+print(type(my_name)) 
+
+# Assigning the value 19 to the variable my_number, is a data type INTEGER. 
+my_number = 19 
+# Printing the data type of the variable my_number. 
+print(type(my_number)) 
+
+# Assigning the value True to the variable is_single, is a data type BOOLEAN. 
+is_single = True 
+# Printing the data type of the variable is_single. 
+print(type(is_single)) 
+
+# Asking the user to input a value. 
+my_input = input('How old are you?\n') 
+# Printing the data type of the variable my_input. 
+
+print(type(my_input))
