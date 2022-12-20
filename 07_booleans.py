107c52ba28fd125e88366220eaa69c5d71d6b98d
JuanK003 <juankdev003@gmail.com>
2022-12-20 17:58:35 -0600

    Booleans

---
+++ 07_booleans.py
@@ -0,0 +1,11 @@
+# Assigning the value True to the variable is_single and then printing the type of the variable.
+is_single = True
+print(type(is_single))
+
+# Printing the opposite of True and False.
+print(not True)
+print(not False)
+
+# Flipping the boolean value of is_single.
+is_single = not is_single
+print(is_single)
