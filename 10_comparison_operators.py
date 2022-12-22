f9ddedebc29a5b4584640fac758512d607fd30be
JuanK003 <juankdev003@gmail.com>
2022-12-21 18:14:35 -0600

    Comparison Operators

---
+++ 10_comparison_operators.py
@@ -0,0 +1,42 @@
+# > → Greater than
+# < → Less than
+# (≥) >= → Greater than or equal
+# (≤) <= → Less than or equal
+# == → Equal than
+# != → Different from
+
+# Checking if 7 is greater than 4.
+print(7 > 4)
+
+# Checking if 7 is less than 4.
+print(7 < 4)
+
+# Checking if 3 is greater than or equal to 4.
+print(3 >= 4)
+
+# Checking if 7 is less than or equal to 66.
+print(7 <= 66)
+
+# Checking if 5 is equal to 5.
+print(5 == 5)
+
+# Checking if 3 is different from 11.
+print(3 != 11)
+
+# Checking if the string 'Apple' is equal to the string 'Samsung' and if the string 'Apple' is equal
+# to the string 'Apple'.
+print('Apple' == 'Samsung')
+print('Apple' == 'Apple')
+
+
+# Checking if the string 'Apple' is equal to the string 'Apple' and if the string 'Samsung' is equal
+# to the string 'samsung'.
+print('Apple' == "Apple")
+print('Samsung' == 'samsung')
+
+# Checking if the string '1' is equal to the integer 1.
+print('1' == 1)
+
+# Checking if the variable age is greater than or equal to 18.
+age = 22
+print(age >= 18)
