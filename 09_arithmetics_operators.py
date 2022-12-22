2a538ff194d70834897af036c07f4717a817d56a
JuanK003 <juankdev003@gmail.com>
2022-12-21 18:00:27 -0600

    Arithmetic Operators

---
+++ 09_arithmetics_operators.py
@@ -0,0 +1,38 @@
+# (+) → Addition
+# (-) → Subtraction
+# (*) → Multiplication
+# (/) → Division
+# (%) → Module or Residue
+# (//) → Integer value division
+# (**) → Exponentiation
+
+# Adding 10 + 10
+print (10 + 10)
+
+# Subtracting 5 from 10
+print (10 - 5)
+
+# Multiplying 10 by 10.
+print (10 * 10)
+
+# Dividing 10 by 2.
+print (10 / 2)
+
+# Finding the remainder of 10 divided by 3.
+print (10 % 3)
+
+# Dividing 10 by 4 and then rounding down to the nearest integer.
+print (10 // 4)
+
+# Raising 10 to the power of 4.
+print (10 ** 4)
+
+# Remember PEMDAS → Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+# P → Parenthesis
+# E → Exponents
+# M → Multiplication
+# D → Division
+# A → Addition
+# S → Subtraction
+
+print( 2 ** 3 + 4 - 19 / 2 //4 )
