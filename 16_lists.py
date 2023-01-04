84ba69926de2122a72fac25e84b87d41a3eb2c09
JuanK003 <juankdev003@gmail.com>
2023-01-04 16:28:23 -0600

    Lists

---
+++ 16_lists.py
@@ -0,0 +1,23 @@
+# Creating a list of numbers.
+numbers = [1,2,3,4]
+# Printing the list of numbers and the type of the list.
+print(numbers)
+print(type(numbers))
+
+#-------------------------------
+# Creating a list of tasks.
+tasks = ['make a dishes', 'play videogames']
+print(tasks)
+
+# Creating a list with different types of data.
+types = [1, True, 'Hi']
+print(types)
+
+# Printing the first element of the list.
+print(numbers[0])
+print(tasks[0])
+print(types[1])
+
+# Changing the first element of the list.
+tasks[0] = 'watch tv series'
+print(tasks)
